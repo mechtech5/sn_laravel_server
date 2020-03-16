59492738cf1c3419075c6c0540892ad1687ddd7a
Ayush Likhar <ayush.likhar@neosofttech.com>
2020-03-16 14:23:34 +0530

    added seeding code

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        'user_id' => 1,
+        'user_id' => rand(1, 10),
         'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
         'body' => $faker->paragraphs($nb = 3, $asText = true)
     ];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\User;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,7 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(PostSeeder::class);
+        factory(User::class, 10)->create();
+
+        factory(Post::class, 3)->create()->each(function ($post) {
+            $comments = factory(Comment::class, 5)->make();
+            $post->comments()->saveMany($comments);
+
+            $tags = factory(Tag::class, 5)->make();
+            $post->tags()->saveMany($tags);
+        });
     }
 }
